21f631d93a4422ff4c1b16961042c1585c90d331
Croelan <croelanjr@cbs-corporation.com.pe>
2015-06-04 23:41:16 -0500

    Se agrego el modelo de los pedidos y las correciones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
+class Empresa < ActiveRecord::Base
+end
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def hello
-  	render text: "Hola Mundo"
-  end
+  	def application
+  		@empresa = Empresa.find(1)
+	end
+#  def hello
+#  	render text: "Hola Mundo"
+#  end
 end
